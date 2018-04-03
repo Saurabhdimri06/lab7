358b6633d30c5899467d3d444c8edd80e30a5e62
dimrisaurabh <dimrisaurabh@gmail.com>
2018-04-03 15:00:06 +0530

    Changes done in hi.cpp a new sum function is added

--- hi.cpp
+++ hi.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	cout<<"Doing lab 7 where we have to make several branching and the learning how to push the merged in our as well as others branch";
 	cout<<"\nLets beginn";
+	sum();//a new function named sum is added in line 7
 	return 0;
 }
 
