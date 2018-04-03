b79c521f884631a9533b045ebab7116de061b032
dimrisaurabh <dimrisaurabh@gmail.com>
2018-04-03 14:51:49 +0530

    Edit done in line 6 comments added

--- hi.cpp
+++ hi.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
 	cout<<"Doing lab 7 where we have to make several branching and the learning how to push the merged in our as well as others branch";
-	cout<<"\nLets beginn";
+	cout<<"\nLets beginn";//commenting in branch 1 i.e test1
 	return 0;
 }
 
