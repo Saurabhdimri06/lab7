036992e6f4223c7a161d4e6b203ff2b97d9ddc10
dimrisaurabh <dimrisaurabh@gmail.com>
2018-04-03 15:03:20 +0530

    Changes done in line 7 a new diffrence function is added commiting in test 7

--- hi.cpp
+++ hi.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	cout<<"Doing lab 7 where we have to make several branching and the learning how to push the merged in our as well as others branch";
 	cout<<"\nLets beginn";
+	diffrence();//a new function named diffrence added in test3
 	return 0;
 }
 
