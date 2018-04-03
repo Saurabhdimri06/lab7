de077dbb36ee0d091963d14f03a38ef7b53fbaed
dimrisaurabh <dimrisaurabh@gmail.com>
2018-04-03 14:47:37 +0530

    Pushing all the data into master branch

---
+++ hi.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Doing lab 7 where we have to make several branching and the learning how to push the merged in our as well as others branch";
+	cout<<"\nLets beginn";
+	return 0;
+}
+
+
