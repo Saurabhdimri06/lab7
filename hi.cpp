59306b1b9ccee63213faf3026e181686949ba429
dimrisaurabh <dimrisaurabh@gmail.com>
2018-04-03 14:57:04 +0530

    Changes done in hi.ccp line 5 and 6 and in DT8.docx

--- hi.cpp
+++ hi.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-	cout<<"Doing lab 7 where we have to make several branching and the learning how to push the merged in our as well as others branch";
-	cout<<"\nLets beginn";//commenting in branch 1 i.e test1
+	cout<<"Doing lab 7 where we have to make several branching and the learning how to push the merged in our as well as others branch";//new comment in line 5 also
+	cout<<"\nLets beginn";//commenting in branch 1 i.e test1 and in branch 2 also
 	return 0;
 }
 
